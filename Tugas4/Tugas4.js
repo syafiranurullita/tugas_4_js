20a8233c9735b31b02cfbf0b9055cc7f7795e24e
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 18:41:46 +0700

    tugas 4 js
    untuk NIOMIC

---
+++ Tugas4/Tugas4.js
@@ -0,0 +1,19 @@
+
+var x = 120;
+var y = 160;
+var z = 190;
+
+if(z>y){
+  if(z>x){
+    console.log("z adalah siswa yang paling tinggi");
+  }else{
+    console.log("Nilai x adalah siswa yang tertinggi kedua");
+  }
+}else{
+    if(x<z){
+      console.log("x adalah siswa yang terpendek");
+    }else{
+      console.log("nilai x adalah yang terpendek kedua");
+    }
+
+  }
